918bf7efc4a1e3479905d45dab71fa9c5e81ce42
Laki-PC\Laki <204-2018@ftn.edu.rs>
2020-12-03 13:11:01 +0100

    Zadatak Restoran

---
+++ DataAccessLayer/RestaurantRepository.cs
@@ -0,0 +1,57 @@
+﻿using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class RestaurantRepository
+    {
+        public List<Restaurant> GettAllRestaurant()
+        {
+            List<Restaurant> all = new List<Restaurant>();
+
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = "SELECT * FROM MenuItems";
+
+                sqlConnection.Open();
+
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                while (sqlDataReader.Read())
+                {
+                    Restaurant r = new Restaurant();
+                    r.Id = sqlDataReader.GetInt32(0);
+                    r.Title = sqlDataReader.GetString(1);
+                    r.Description = sqlDataReader.GetString(2);
+                    r.Price = sqlDataReader.GetDecimal(3);
+
+                    all.Add(r);
+                }
+
+            }
+
+            return all;
+        }
+
+        public int InsertMenuItems(Restaurant r)
+        {
+            using(SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = String.Format("INSERT INTO MenuItems VALUES ('{0}', '{1}', {2})", r.Title, r.Description, r.Price);
+
+                sqlConnection.Open();
+
+                return sqlCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
